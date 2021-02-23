5b75716f1cac091c55ad3be40b8d955c24940ef1
kkomaweb83 <kkomaweb@nate.com>
2021-02-23 11:16:32 +0900

    주문시스템 V0.01 - 역할과 구현을 충실하게 분리했다 - 다형성도 활용하고, 인터페이스와 구현 객체를 분리했다 - OCP, DIP 같은 객체지향 설계 원칙을 충실히 준수했나? * 그렇게 보이지만 사실은 아니다

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,31 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class MemberApp {
+	
+	public static void main(String[] args) {
+		MemberService memberService = new MemberServiceImpl();
+		OrderService orderService = new OrderServiceImpl();
+		
+		long memberId = 1L;
+		
+		Member member = new Member(memberId, "meberA", Grade.VIP);
+		memberService.join(member);
+		
+//		Member findMember = memberService.findMember(1L);
+//		System.out.println("new member = " + member.getName());
+//		System.out.println("find member = " + findMember.getName());
+		
+		Order order = orderService.createOrder(memberId, "itemA", 10000);
+		
+		System.out.println("order = " + order);
+	}
+	
+}
